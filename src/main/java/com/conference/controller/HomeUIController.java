a5312732bdd12f3a9a8da95fc279af267da1af3c
fedorchuksveta <fedrchuksveta@gmail.com>
2019-09-06 13:21:22 +0300

    Changed controller - register. Created registerUserSuccess.

--- src/main/java/com/conference/controller/HomeUIController.java
+++ src/main/java/com/conference/controller/HomeUIController.java
@@ -6,9 +6,15 @@ import com.conference.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
+import java.util.List;
 
 
 @Controller
@@ -31,14 +37,14 @@ public class HomeUIController {
 //        return "roomPresentation";
 //    }
 
-    @GetMapping("/presentation/")
+    @GetMapping("/presentation")
     public String homePage(Model model) {
         model.addAttribute("presentation", presentationService.findAll());
         return "presentation";
     }
 
 
-    @RequestMapping("/register/")
+    @RequestMapping("/register")
     public String registerCustomer(Model model) {
 
         User user = new User();
@@ -48,4 +54,17 @@ public class HomeUIController {
 
     }
 
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public String registerCustomerPost(@Valid @ModelAttribute("user") User user, BindingResult result,
+                                       Model model) {
+
+        if (result.hasErrors()) {
+            return "registerUser";
+        }
+
+        userService.create(user);
+
+        return "registerUserSuccess";
+
+    }
 }
